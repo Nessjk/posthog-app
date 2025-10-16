23fd415109816c5326eb1bafbd357024485821c3
nessjk <nessjk@gmail.com>
2025-10-16 16:25:37 +0700

    comments - update info on different parts of module parameters

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,7 +1,8 @@
 const modules = import.meta.glob("/src/assets/icons/*.png", {
-  eager: true,
-  as: "url",
+  eager: true, // no lazy loading
+  as: "url", // not sure but seem to return optimised public url for each img
 }) as Record<string, string>;
 
+// helper function: give the "doc" key returns doc.png
 export const iconUrl = (key: string): string | undefined =>
   modules[`/src/assets/icons/${key}.png`];
