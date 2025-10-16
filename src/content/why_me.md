84a81528416363edc64c3390e68e4aaaece3392a
nessjk <nessjk@gmail.com>
2025-10-16 12:07:53 +0700

    first commit & store start

---
+++ src/content/why_me.md
@@ -0,0 +1,9 @@
+# Why me?
+
+## hi there I'm tony
+
+Paragraph about why and hi!
+
+## reasons why I'll bring value technically, culturaly, financially to your organisation
+
+## why i'm a good fit
---
+++ src/data/items.ts
@@ -0,0 +1,29 @@
+export type ItemKind = "doc" | "folder" | "link";
+export type DesktopItem = {
+  id: string;
+  name: string;
+  kind: ItemKind;
+  icon: string;
+  children?: string[];
+  initialMarkdown?: string; // for docs
+  initialContent?: string; // HTML fallback
+};
+
+import why_me_md from "../content/why_me.md?raw";
+
+export const desktopItems: DesktopItem[] = [
+  {
+    id: "why-me",
+    name: "Why me?",
+    kind: "doc",
+    icon: "doc",
+    initialMarkdown: why_me_md,
+  },
+  {
+    id: "side-projects",
+    name: "Side Projects",
+    kind: "folder",
+    icon: "folder",
+    children: ["link", "link2", "doc"],
+  },
+];
